59599ea02a80390935b1e59566b900e8e682488e
Rgarlay <ravigarlay@gmail.com>
2024-10-23 18:12:03 +0530

    Project all bone structure

---
+++ src/news_project/utils.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from src.news_project.exception import CustomException
+from src.news_project.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+import pymysql
+
+import pickle
+import numpy as np
+
+load_dotenv()
+
+    
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
